a497adf19e5cdc0908f566785784b518f97f7e27
luki120 <luki120@exerra.xyz>
2024-07-12 20:32:04 -0300

    optimized imports

--- src/main/kotlin/com/luki/Application.kt
+++ src/main/kotlin/com/luki/Application.kt
@@ -1,11 +1,12 @@
 package com.luki
 
-import com.luki.plugins.*
+import com.luki.plugins.configureMonitoring
+import com.luki.plugins.configureRouting
+import com.luki.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = EngineMain.main(args)
+fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
 	configureSerialization()
