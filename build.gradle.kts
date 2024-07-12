ae9111d4ed4ef0fe282191a92e0ed89b054f8097
luki120 <luki120@exerra.xyz>
2024-07-12 20:28:55 -0300

    better [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,20 +10,20 @@ version = "0.9.0"
 application {
 	mainClass.set("io.ktor.server.netty.EngineMain")
 
-	val isDevelopment: Boolean = project.ext.has("development")
+	val isDevelopment = project.ext.has("development")
 	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+dependencies {
+	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+	implementation("io.ktor:ktor-server-core-jvm")
+	implementation("io.ktor:ktor-server-call-logging-jvm")
+	implementation("io.ktor:ktor-server-content-negotiation-jvm")
+	implementation("io.ktor:ktor-server-host-common-jvm")
+	implementation("io.ktor:ktor-server-netty-jvm")
+	implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 repositories {
 	mavenCentral()
-}
-
-dependencies {
-	implementation("io.ktor:ktor-server-content-negotiation-jvm")
-	implementation("io.ktor:ktor-server-core-jvm")
-	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-	implementation("io.ktor:ktor-server-call-logging-jvm")
-	implementation("io.ktor:ktor-server-host-common-jvm")
-	implementation("io.ktor:ktor-server-netty-jvm")
-	implementation("ch.qos.logback:logback-classic:1.5.6")
 }
