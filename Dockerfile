55aaf29df39014be5bc50550d6e6c6e6dc62ac83
luki120 <luki120@exerra.xyz>
2025-10-04 00:11:12 -0300

    smh

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM gradle AS build
+FROM gradle:9-jdk21 AS build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle buildFatJar --no-daemon
 
-FROM openjdk:21
+FROM openjdk:21-jdk-slim
 EXPOSE 8080:8080
 RUN mkdir /app
 COPY --from=build /home/gradle/src/build/libs/*.jar /app/Ianthea.jar
--- src/main/kotlin/me/luki/data/model/subject/Subject.kt
+++ src/main/kotlin/me/luki/data/model/subject/Subject.kt
@@ -2,6 +2,7 @@ package me.luki.data.model.subject
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
@@ -17,19 +18,32 @@ data class Subject(
 	val finalGrades: List<Int> = emptyList(),
 	val isFinished: Boolean = false,
 	val hasThreeExams: Boolean = false,
-	@Serializable(with = LocalDateSerializer::class)
-	val finalExamDate: LocalDate = LocalDate.now()
+    @Serializable(with = LocalDateListSerializer::class)
+	val finalExamDates: List<LocalDate> = emptyList()
 )
 
 private object LocalDateSerializer: KSerializer<LocalDate> {
-	override val descriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+    override val descriptor = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
 
-	override fun deserialize(decoder: Decoder): LocalDate {
-		val dateString = decoder.decodeString()
-		return LocalDate.parse(dateString)
-	}
+    override fun deserialize(decoder: Decoder): LocalDate {
+        val dateString = decoder.decodeString()
+        return LocalDate.parse(dateString)
+    }
 
-	override fun serialize(encoder: Encoder, value: LocalDate) {
-		encoder.encodeString(value.toString())
-	}
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.toString())
+    }
+}
+
+private object LocalDateListSerializer : KSerializer<List<LocalDate>> {
+    private val listSerializer = ListSerializer(LocalDateSerializer)
+    override val descriptor = listSerializer.descriptor
+
+    override fun deserialize(decoder: Decoder): List<LocalDate> {
+        return listSerializer.deserialize(decoder)
+    }
+
+    override fun serialize(encoder: Encoder, value: List<LocalDate>) {
+        listSerializer.serialize(encoder, value)
+    }
 }
